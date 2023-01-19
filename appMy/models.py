7b619e0b329c2832aae47134c07ddf874ac5f9e1
Berkay Ülger <berkayulger45920@gmail.com>
2023-01-19 19:30:33 +0300

    -

--- appMy/models.py
+++ appMy/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Card(models.Model):
+    brand = models.CharField(("Marka"), max_length=50, null=True)
     title = models.CharField(("Başlık"), max_length=50)
     text = models.TextField(("İçerik"), max_length=500)
     date_now = models.DateTimeField(("Tarih Saat"), auto_now_add=False)
+    price = models.IntegerField(("Fiyat"), null=True)
     
     def __str__(self):
         return self.title
